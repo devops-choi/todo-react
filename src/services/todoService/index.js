11be2eedcbaf8166c9971fb7ea77d105ea3896ea
Choi Won <help@myso.kr>
2025-06-13 21:54:10 +0900

    refactor: clean up todoService exports for better readability

--- src/services/todoService/index.js
+++ src/services/todoService/index.js
@@ -3,17 +3,12 @@ import { selectTodoItem } from "./selectTodoItem";
 import { createTodoItem } from "./createTodoItem";
 import { updateTodoItem } from "./updateTodoItem";
 import { deleteTodoItem } from "./deleteTodoItem";
-export {
+const todoService = {
     selectTodoList,
     selectTodoItem,
     createTodoItem,
     updateTodoItem,
     deleteTodoItem
 };
-export default {
-    selectTodoList,
-    selectTodoItem,
-    createTodoItem,
-    updateTodoItem,
-    deleteTodoItem
-};
+export default todoService;
+export { selectTodoList, selectTodoItem, createTodoItem, updateTodoItem, deleteTodoItem };
